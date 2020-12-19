3ed1abde360f916c9eb8e581789a79f80913d24d
Jay Wilder <jayswilder@yahoo.com>
2020-12-19 10:47:55 -0600

    initial upload

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import cars from '../cars.json'
-import { Container, Paper, Chip } from '@material-ui/core';
+import React from "react";
+import cars from "../cars.json";
+import { Container, Paper, Chip, Button } from "@material-ui/core";
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import "../App.css";
+
 
 const Car = (props) => {
-    const id = props.match.params.id
-    const car = cars.find(c => c.id == id)
+    const id = props.match.params.id;
+    const car = cars.find(c => c.id == id);
 
     return (
         <Container maxWidth="sm" className="car-container">
             <Paper className="car-paper">
-                <h2>{car.Name}</h2>
-                {
-                    Object.keys(car).map((key, idx) => {
-                        return <Chip label={`${key}: ${car[key]}`}></Chip>
-                    })
-                }
+                <h2 style={{
+                    textTransform: "uppercase"
+                }}>{car.Name}</h2>
+                {Object.keys(car).map((key, i) => {
+                    return <Chip label={`${key}: ${car[key]}`}></Chip>;
+                })}
+
+                <br></br>
+                <br></br>
+
+                <a href="/">
+                    <Button className="backButton" variant="contained" color="primary">
+                        <ArrowBackIosIcon fontSize="small" /> Back
+                </Button>
+                </a>
+
             </Paper>
         </Container>
-    )
-}
+    );
+};
 
-export default Car
+export default Car;
